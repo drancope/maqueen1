5fd874002a8ca2a4bdbde239a7da3ebbe64150b6
Antonio Contreras <drancope@gmail.com>
2025-04-01 12:33:45 +0200

    corregido

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 0) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
         maqueen.motorStop(maqueen.Motors.All)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+        basic.pause(500)
     } else {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 129)
     }
     basic.showNumber(maqueen.Ultrasonic(PingUnit.Centimeters))
 })
